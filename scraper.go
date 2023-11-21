91152979077cf2fb1710d5b8fbd33f6549002e45
abramq <abram@wp.pl>
2023-11-21 17:39:02 +0000

    ok

--- scraper.go
+++ scraper.go
@@ -1,71 +1,25 @@
 package main
 
 import (
-	"encoding/csv"
-	"log"
-	"os"
+	"fmt"
 
 	"github.com/gocolly/colly"
 )
 
-// initializing a data structure to keep the scraped data
-type PokemonProduct struct {
-	url, image, name, price string
-}
-
 func main() {
-	// initializing the slice of structs to store the data to scrape
-	var pokemonProducts []PokemonProduct
-
-	// creating a new Colly instance
+	// Instantiate default collector
 	c := colly.NewCollector()
 
-	// visiting the target page
-	c.Visit("https://scrapeme.live/shop/")
-
-	// scraping logic
-	c.OnHTML("li.product", func(e *colly.HTMLElement) {
-		pokemonProduct := PokemonProduct{}
-
-		pokemonProduct.url = e.ChildAttr("a", "href")
-		pokemonProduct.image = e.ChildAttr("img", "src")
-		pokemonProduct.name = e.ChildText("h2")
-		pokemonProduct.price = e.ChildText(".price")
-
-		pokemonProducts = append(pokemonProducts, pokemonProduct)
+	// On every a element which has href attribute call callback
+	c.OnHTML(".col-md-3 span", func(e *colly.HTMLElement) {		
+		fmt.Printf("This found: %q -> %s\n", e.Text )
 	})
 
-	// opening the CSV file
-	file, err := os.Create("products.csv")
-	if err != nil {
-		log.Fatalln("Failed to create output CSV file", err)
-	}
-	defer file.Close()
-
-	// initializing a file writer
-	writer := csv.NewWriter(file)
-
-	// writing the CSV headers
-	headers := []string{
-		"url",
-		"image",
-		"name",
-		"price",
-	}
-	writer.Write(headers)
-
-	// writing each Pokemon product as a CSV row
-	for _, pokemonProduct := range pokemonProducts {
-		// converting a PokemonProduct to an array of strings
-		record := []string{
-			pokemonProduct.url,
-			pokemonProduct.image,
-			pokemonProduct.name,
-			pokemonProduct.price,
-		}
-
-		// adding a CSV record to the output file
-		writer.Write(record)
-	}
-	defer writer.Flush()
+	// Before making a request print "Visiting ..."
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL.String())
+	})
+	
+	// Start scraping on https://www.mosir.zgora.pl/
+	c.Visit("https://www.mosir.zgora.pl/")
 }
