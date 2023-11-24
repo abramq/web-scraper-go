671d1bab3f2546a7d65af819a505a4a12ce02d9d
abramq <abram@wp.pl>
2023-11-24 17:23:29 +0000

    RegExp rulez :-)

--- scraper.go
+++ scraper.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"strings"
+	"regexp"
 
 	"github.com/gocolly/colly"
 )
@@ -9,12 +11,25 @@ import (
 func main() {
 	// Instantiate default collector
 	c := colly.NewCollector()
-
+	cOpis := ""
+	cOpisNew1 := ""
+	cOpisNew2 := ""
+	
 	// On every a element which has href attribute call callback
-	c.OnHTML(".col-md-3 span", func(e *colly.HTMLElement) {		
-		fmt.Printf("This found: %q -> %s\n", e.Text )
-	})
+	c.OnHTML("section#count div.container div.row div.col-md-3", func(e *colly.HTMLElement) {	
+		cOpis = e.Text
 
+		reg, _ := regexp.Compile("[^a-zA-Z]+ ")	
+		cOpisNew1 = reg.ReplaceAllString(cOpis, "")
+		cOpisNew1 = strings.Trim(cOpisNew1, " ")
+		fmt.Println("txt->",cOpisNew1,"<-")
+
+		reg, _ = regexp.Compile("[^0-9]+")	
+		cOpisNew2 = reg.ReplaceAllString(cOpis, "")
+		cOpisNew2 = strings.Trim(cOpisNew2, " ")
+		fmt.Println("num->",cOpisNew2,"<-")
+	})	
+	
 	// Before making a request print "Visiting ..."
 	c.OnRequest(func(r *colly.Request) {
 		fmt.Println("Visiting", r.URL.String())
